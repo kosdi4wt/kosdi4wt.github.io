d5bccec88055a8e3becd74a6104921421ca1d93e
kosdi4wt <1933372264@qq.com>
2017-12-07 10:46:45 +0800

    提交

---
+++ 微场景/js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+});
