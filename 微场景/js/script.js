d21da0901bafa1cc1f95f098e359d57be1889649
kosdi4wt <1933372264@qq.com>
2017-12-12 17:20:45 +0800

    音乐

--- 微场景/js/script.js
+++ 微场景/js/script.js
@@ -20,3 +20,19 @@ var swiper = new Swiper('.swiper-container', {
         } 
       }
 });
+var mp3=document.querySelector("#mp3");
+var mp3btn=document.querySelector(("#mp3btn"));
+
+// play pause 俩个属性控制audio 的播放和暂停
+// running  paused 俩个属性控制 动画的播放和暂停
+// 绑定标签 定义点击事件 如果音乐是暂停的 那么点击播放 不是则点击暂停
+// 动画：使用js添加css样式，animationPlayState（=animation-play-state）控制动画的播放和暂停
+function playPause() {
+    if(mp3.paused){
+        mp3.play();
+        mp3btn.style.animationPlayState="running";
+    }else{
+        mp3.pause();
+        mp3btn.style.animationPlayState="paused";
+    }
+}
